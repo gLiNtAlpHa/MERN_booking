b86facd268cd5bfb0c8b8fcb74d691c3bf1be3b2
gLiNtAlpHa <sesaalolu@gmail.com>
2023-02-11 19:53:56 +0100

    hotelscontrollers handled

--- Backend_API/routes/hotels.js
+++ Backend_API/routes/hotels.js
@@ -1,65 +1,23 @@
 import express from "express";
-import Hotel from "../models/Hotel.js";
-import { createError } from "../utils/error.js";
+import {
+	createHotel,
+	deleteHotel,
+	getHotel,
+	getHotels,
+	updateHotel,
+} from "../controllers/hotelsController.js";
+
 const router = express.Router();
 
 //create HOTEL
 
-router.post("/", async (req, res) => {
-	const newHotel = new Hotel(req.body);
-
-	try {
-		const savedHotel = await newHotel.save();
-		res.status(200).json(savedHotel);
-	} catch (err) {
-		res.status(500).json(err);
-	}
-});
+router.post("/", createHotel);
 // uUPDATE HOTEL
-router.put("/:id", async (req, res) => {
-	try {
-		const updatedHotel = await Hotel.findByIdAndUpdate(
-			req.params.id,
-			{
-				$set: req.body,
-			},
-			{
-				new: true,
-			}
-		);
-		res.status(200).json(updatedHotel);
-	} catch (err) {
-		res.status(500).json(err);
-	}
-});
+router.put("/:id", updateHotel);
 //Delete HOTEL
-router.delete("/:id", async (req, res) => {
-	try {
-		await Hotel.findByIdAndDelete(req.params.id);
-		res.status(200).json("Hotel delected");
-	} catch (err) {
-		res.status(500).json(err);
-	}
-});
+router.delete("/:id", deleteHotel);
 // GET HOTEL
-router.get("/:id", async (req, res, next) => {
-	// return next();
-	try {
-		const hotel = await Hotel.findById(req.params.id);
-		res.status(200).json(hotel);
-	} catch (err) {
-		res.status(500).json(err);
-	}
-});
+router.get("/:id", getHotel);
 // GET ALL HOTELS
-router.get("/", async (req, res) => {
-	// const failed = true;
-	// if (failed) return next(createError(401, "you are not authenticated"));
-	try {
-		const hotels = await Hotel.find();
-		res.status(200).json(hotels);
-	} catch (err) {
-		res.status(500).json(err);
-	}
-});
+router.get("/", getHotels);
 export default router;
